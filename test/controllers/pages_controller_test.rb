77ebba9f5bf4606f36b785f355b8147978e3ad71
dl-watson <dlarkinwatson@gmail.com>
2021-06-01 11:15:56 -0700

    failing test for title selector

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -4,15 +4,18 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get pages_home_url
     assert_response :success
+    assert_select "title", "Home | Rails Sample App"
   end
 
   test "should get help" do
     get pages_help_url
     assert_response :success
+    assert_select "title", "Help | Rails Sample App"
   end
 
   test "should get about" do
     get pages_about_url
     assert_response :success
+    assert_select "title", "About | Rails Sample App"
   end
 end
