2342619dfb602d43be69633f4d8e729c0064ceea
dl-watson <dlarkinwatson@gmail.com>
2021-06-01 09:33:39 -0700

    swoop

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -10,4 +10,9 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
     get pages_help_url
     assert_response :success
   end
+
+  test "should get about" do
+    get pages_about_url
+    assert_response :success
+  end
 end
